5d9ce941c5cabc22291c175090cbc7aa7bea4fd4
yixianzi <44867804+yixianzi@users.noreply.github.com>
2019-10-20 14:39:44 +0800

    Updates

---
+++ precache-manifest.438327bbe97cc0f0cd40473c5814d124.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03624cfc18413a364989ff70b8268e2f",
+    "url": "/gxy-love-lhy/index.html"
+  },
+  {
+    "revision": "d3f39a5186f9f8065d2b",
+    "url": "/gxy-love-lhy/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "1a998cb5e40737bb301b",
+    "url": "/gxy-love-lhy/static/css/main.e5571238.chunk.css"
+  },
+  {
+    "revision": "d3f39a5186f9f8065d2b",
+    "url": "/gxy-love-lhy/static/js/2.0fe4854d.chunk.js"
+  },
+  {
+    "revision": "1a998cb5e40737bb301b",
+    "url": "/gxy-love-lhy/static/js/main.ff541b3f.chunk.js"
+  },
+  {
+    "revision": "c8c315bb12c2fa1735dd",
+    "url": "/gxy-love-lhy/static/js/runtime-main.0f17a4f5.js"
+  },
+  {
+    "revision": "8e22783d707ad140bffe18b2a3812529",
+    "url": "/gxy-love-lhy/static/media/digital-7_mono.8e22783d.ttf"
+  }
+]);
